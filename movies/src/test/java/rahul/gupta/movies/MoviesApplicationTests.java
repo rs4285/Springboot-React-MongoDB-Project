3462876b003b90ee2350552c307915c1073277ed
Rahul Gupta <104451837+rs4285@users.noreply.github.com>
2024-09-25 14:22:42 +0530

    Backend

---
+++ movies/src/test/java/rahul/gupta/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package rahul.gupta.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
